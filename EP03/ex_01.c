063169e442550345417af3b00d3046dfc2077a1b
Itravain <icarotravain@gmail.com>
2023-04-12 11:31:21 -0300

    Comentários

--- EP03/ex_01.c
+++ EP03/ex_01.c
@@ -1,20 +1,22 @@
 #include <stdio.h>
 
-int leitura(int array[10]){
-    int i;
-    for(i=0; i<10; i++){
-        printf("Digite o %dº número: ", i+1);
-        scanf("%d", &array[i]);
-    }
-    
-}
-
+//Este algoritmo substitui todos os valores nulos e negativos de um vetor por 1
 int main(){
     int array[10];
+    int i;
 
+    //Lê os valores do vetor
+    for (i = 0; i < 10; i++){
+        scanf("%d", &array[i]);
+    }
 
-
-
+    //Substitui os valores nulos e negativos por 1
+    for (i = 0; i < 10; i++){
+        if (array[i] <= 0){
+            array[i] = 1;
+        }
+        printf("X[%d] = %d\n", i, array[i]);
+    }
 
     return 0;
 }
