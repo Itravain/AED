e0577541176865d161f58527648e25f8c5e1a083
Itravain <icarotravain@gmail.com>
2023-04-12 12:36:00 -0300

    ex_2 1atualização

---
+++ EP03/ex_02.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    char List1[50], List2[50], List3[100];
+    int i;
+
+    printf("Digite os nomes da lista atual:");
+    scanf("%s[^\n]", &List1[i]);
+    
+
+    printf("%s", List1[i]);
+
+    
+
+
+
+
+    return 0;
+}
