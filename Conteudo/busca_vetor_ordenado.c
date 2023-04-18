5d4e347c4ae9384a14f208fb975e57ea17775c27
Itravain <icarotravain@gmail.com>
2023-04-18 00:27:10 -0300

    Innicio do algoritmo de ordenacao

---
+++ Conteudo/busca_vetor_ordenado.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <time.h>
+
+int buscar_ordem(int v[], int n, int x){
+	int i = 0;
+
+	while(i<n && i != x)i++;
+ 	return i;
+}
+
+
+int main(void){
+	int i = 0;
+
+	printf("%d\n", i);
+
+
+
+	return 0;
+}
