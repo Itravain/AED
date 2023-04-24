f58a87c1a4fb28d1ce5e4ac122c4b670b28fb2cf
Itravain <icarotravain@gmail.com>
2023-04-24 10:55:39 -0300

    nergeSort(V2)

--- Atividades/EP04/mergeSort.c
+++ Atividades/EP04/mergeSort.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 #include <math.h>
 
-void merge(char **V, int inicio, int meio, int fim){
+void merge(char **Words, int inicio, int meio, int fim){
 	char **temp;
 	int p1, p2, tamanho, i, j, k;
 	int fim1 = 0, fim2 = 0;
@@ -14,35 +14,35 @@ void merge(char **V, int inicio, int meio, int fim){
 	if(temp != NULL){
 		for(i = 0; i<tamanho; i++){
 			if(!fim1 && !fim2){
-				if(strcmp(V[p1], V[p2]) < 0){
-					temp[i] = V[p1++];
+				if(strcmp(Words[p1], Words[p2]) < 0){
+					temp[i] = Words[p1++];
 				}
 				else{
-					temp[i] = V[p2++];
+					temp[i] = Words[p2++];
 				}
 				if(p1>meio) fim1=1;
 				if(p2>fim) fim2=1;
 			}else{
 				if(!fim1)
-					temp[i] = V[p1++];
+					temp[i] = Words[p1++];
 				else
-					temp[i] = V[p2++];
+					temp[i] = Words[p2++];
 			}
 		}
 		for(j=0, k=inicio; j<tamanho; j++, k++){
-			V[k] = temp[j];
+			Words[k] = temp[j];
 		}
 	}
 	free(temp);
 }
 
-void mergeSort(char **V, int inicio, int fim){
+void mergeSort(char **Words, int inicio, int fim){
     int meio;
     if (inicio<fim){
         meio = floor((inicio+fim)/2);
-        mergeSort(V, inicio, meio);
-        mergeSort(V, meio+1, fim);
-        merge(V, inicio, meio, fim);
+        mergeSort(Words, inicio, meio);
+        mergeSort(Words, meio+1, fim);
+        merge(Words, inicio, meio, fim);
     }
 }
 
@@ -57,7 +57,7 @@ int main (void){
     getchar();
 
     nomes = (char **) malloc(qt*sizeof(char *));
-    for ( i = 0; i < qt; i++)
+    for (i = 0; i < qt; i++)
     {
         //leitura dos nomes
         nomes[i] = (char *) malloc(50*sizeof(char));
@@ -69,7 +69,7 @@ int main (void){
 
     printf("%s", nomes[pos-1]);
 
-    for ( i = 0; i < qt; i++)
+    for (i = 0; i < qt; i++)
     {
         free(nomes[i]);
     }
