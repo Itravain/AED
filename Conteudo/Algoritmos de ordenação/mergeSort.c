9ef932380309f16928e2e8e276878a1d739946a8
Itravain <icarotravain@gmail.com>
2023-04-23 23:45:52 -0300

    merge

--- Conteudo/Algoritmos de ordenação/mergeSort.c
+++ Conteudo/Algoritmos de ordenação/mergeSort.c
@@ -14,11 +14,33 @@ void mergeSort(int *V, int inicio, int fim){
 }
 
 void merge(int *V, int inicio, int meio, int fim){
+	int *temp, p1, p2, tamanho, i, j, k;
+	int fim1 = 0, fim 2 = 0;
+	tamanho = fim-inicio+1;
+	p1 = inicio;
+	p2 = meio +1;
+	temp = (int *) malloc(tamanho*sizeof(int));
+	if(temp != NULL){
+		for(i = 0; i<tamanho; i++){
+			if(!fim1 && !fim2){
+				if(V[p1] < V[p2]){
+					temp[i]= V[p1++];
+				}
+				else{
+					temp[i] = V[p2++];
+				}
+				if(p1>meio) fim1=1;
+				if(p2>meio) fim2=2;
+			}
+		}
+	}
+			else{
+				if(!fim1)
+					temp[i]= V[p1++];
+				else
+					temp[i]= V[p2++];
+			}
 
-
-
-    
-}
 
 
 int main(void){
