b1a1fad53f7ebd5ab8365383df6a39431016e444
Itravain <icarotravain@gmail.com>
2023-05-21 21:46:44 -0300

    EP07

--- Atividades/EP03/ex_01.c
+++ Atividades/EP03/ex_01.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
 
-//Este algoritmo substitui todos os valores nulos e negativos de um vetor por 1
-int main(){
-    int array[10];
-    int i;
+// Este algoritmo substitui todos os valores nulos e negativos de um vetor por 1
+int main() {
+  int array[10];
+  int i;
 
-    //Lê os valores do vetor
-    for (i = 0; i < 10; i++){
-        scanf("%d", &array[i]);
+  // Lê os valores do vetor
+  for (i = 0; i < 10; i++) {
+    scanf("%d", &array[i]);
+  }
+
+  // Substitui os valores nulos e negativos por 1
+  for (i = 0; i < 10; i++) {
+    if (array[i] <= 0) {
+      array[i] = 1;
     }
+    printf("X[%d] = %d\n", i, array[i]);
+  }
 
-    //Substitui os valores nulos e negativos por 1
-    for (i = 0; i < 10; i++){
-        if (array[i] <= 0){
-            array[i] = 1;
-        }
-        printf("X[%d] = %d\n", i, array[i]);
-    }
-
-    return 0;
+  return 0;
 }
---
+++ Atividades/EP07/arv_v1.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct noarv {
+  char letra;
+  struct noarv *esquerda;
+  struct noarv *direita;
+
+} NoArv;
+
+int ind_in_order(char in_order[], char letra){
+  int cont = 0;
+  while (in_order[cont] != letra){
+    cont++;
+  }
+  return cont;
+}
+
+void insert(NoArv **raiz, char letra, char in_order[]) {
+  NoArv* novo_no = *raiz;
+  while(novo_no){
+    if(ind_in_order(in_order, letra) < ind_in_order(in_order, novo_no->letra)){
+      raiz = &novo_no->esquerda;
+    }
+    else{
+      raiz = &novo_no->direita;
+    }
+    novo_no = *raiz;
+  }
+  novo_no = malloc(sizeof(NoArv));
+  novo_no->direita = NULL;
+  novo_no->esquerda = NULL;
+  novo_no->letra = letra;
+  *raiz = novo_no;
+}
+
+void imprimir_edr(NoArv *raiz) {
+  if(raiz){
+    imprimir_edr(raiz->esquerda);
+    imprimir_edr(raiz->direita);
+    printf("%c", raiz->letra);
+  }
+  
+}
+
+int main() {
+  NoArv* raiz;
+  int qtd, rep, i, j;
+  char* pre_order;
+  char* in_order;
+
+  //printf("Quantas vezes o programa irá se repetir: ");
+  scanf("%d", &rep);
+
+  for (i = 0; i < rep; i++){
+    raiz = NULL;
+    pre_order = malloc(sizeof(char) * (2001));
+    in_order = malloc(sizeof(char) * (2001));
+
+    //printf("Tam pre in: ");
+    scanf("%d %s %s", &qtd, pre_order, in_order);
+    
+    //printf("Os caracteres digitados foram: \n%s\n%s\n", pre_order, in_order);
+    for (j = 0; j < qtd; j++)
+    {
+      insert(&raiz, pre_order[j], in_order);
+    }
+    
+    imprimir_edr(raiz);
+    printf("\n");
+
+    free(raiz);
+    free(pre_order);
+    free(in_order);
+  }
+  
+
+  return 0;
+}
