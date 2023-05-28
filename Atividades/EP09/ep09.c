d09b12cfca6b0e31746049f39bae0feb62814fbd
Itravain <icarotravain@gmail.com>
2023-05-28 16:15:14 -0300

    v_finalep09

--- Atividades/EP09/ep09.c
+++ Atividades/EP09/ep09.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void imprimir(char **mapa, int coluna, int linha){
+void matriz(int linha, int coluna, char ***matriz) {
+  int i, j;
+
+  *matriz = malloc(sizeof(char *) * linha);
+
+  for (i = 0; i < linha; i++) {
+    (*matriz)[i] = malloc(sizeof(char) * coluna);
+  }
+}
+
+void imprimir(char **mapa, int coluna, int linha) {
   int i, j;
   for (i = 0; i < linha; i++) {
     for (j = 0; j < coluna; j++) {
@@ -11,82 +21,107 @@ void imprimir(char **mapa, int coluna, int linha){
   }
 }
 
-void matriz(int linha, int coluna, char **matriz){
-  int i, j;
-
-  matriz = malloc(sizeof(char*) * linha);
-
-  for (i = 0; i < linha; i++) {
-    matriz[i] = malloc(sizeof(char) * coluna);
-  }
-}
-
-char verificacao(char **mapa, int coluna, int linha){
+char verificacao(char **mapa, int coluna, int linha) {
   int j = 0, i = 0;
-  char comando;
+  char comando = mapa[0][0];
   char **verificacao = NULL;
 
-  matriz(linha, coluna, verificacao);
+  matriz(linha , coluna , &verificacao);
 
   while (comando != '*') {
-    if (mapa[i][j] == '.' && comando == '>'){
-      j++;
-    }
-    else if (mapa[i][j] == '.' && comando == '<'){
-      j--;
-    }
-    else if (mapa[i][j] == '.' && comando == 'v'){
-      i++;
-    }
-    else if (mapa[i][j] == '.' && comando == '^'){
-      i--;
-    }
-    else if (mapa[i][j] == '>'){
-      comando = '>';
-      if (verificacao[i][j] == comando){
+    //condições de .
+    if (mapa[i][j] == '.' && comando == '>') {
+      if (j == coluna - 1){
         return comando;
       }
-      else{
-        verificacao[i][j] = comando;
-      }
       j++;
     }
-    else if (mapa[i][j] == '<'){
-      comando = '<';
-      verificacao[i][j] = '<';
+    else if (mapa[i][j] == '.' && comando == '<') {
+      if(j == 0){
+        return comando;
+      }   
       j--;
+    } 
+    else if (mapa[i][j] == '.' && comando == 'v') {
+      if(i == linha - 1){
+        return comando;
+      }
+      i++;
     }
-    else if (mapa[i][j] == '^'){
-      comando = '^';
-      verificacao[i][j] = '^';
+    else if (mapa[i][j] == '.' && comando == '^') {
+      if(i == 0){
+        return comando;
+      }
       i--;
     }
-    else if (mapa[i][j] == 'v'){
-      comando = 'v';
-      verificacao[i][j] = 'v';
-      i++;
+
+    //verificação de loop
+    else {
+      comando = mapa[i][j];
+      if (verificacao[i][j] == comando) {
+        return comando;
+      }
+      else {
+        verificacao[i][j] = comando;
+      }
+
+
+      //verificação dos comandos
+      if (comando == '>'){
+        if(j == coluna - 1){
+          return comando;
+        }
+        j++;
+      }
+      else if (comando == '<'){
+        if(j == 0){
+          return comando;
+        }
+        j--;
+      }
+      else if (comando == '^'){
+        if (i == 0){
+          return comando;
+        }   
+        i--;
+      }
+      else if (comando == 'v'){
+        if (i == linha - 1){
+          return comando;
+        }
+        i++;
+      }
     }
   }
   return comando;
 }
 
-int main (){
+int main() {
   int coluna, linha, i, j;
-  char** mapa = NULL;
+  char **mapa = NULL;
+  //printf("Digite o número de colunas: ");
   scanf("%d", &coluna);
+  //printf("Digite o número de linhas: ");
   scanf("%d", &linha);
   getchar();
 
-  matriz(linha, coluna, mapa);
-  
+  matriz(linha , coluna , &mapa);
+
   for (i = 0; i < linha; i++) {
     for (j = 0; j < coluna; j++) {
       scanf("%c", &mapa[i][j]);
-      getchar();
     }
+    getchar();
   }
 
-  imprimir(mapa, coluna, linha); 
+  //imprimir(mapa, coluna, linha);
+  if (verificacao(mapa, coluna, linha) != '*'){
+    printf("!\n");
+  }
+  else{
+    printf("%c\n",verificacao(mapa, coluna, linha));
+  }
+  
 
   return 0;
 }
