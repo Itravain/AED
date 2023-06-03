3b4dd5c99453e924141f126c1c7bf96232f14ec5
Itravain <icarotravain@gmail.com>
2023-06-02 21:33:32 -0300

    Atualizacao conteudo arvore

--- Conteudo/Estruturas de dados/arvore_binaria.c
+++ Conteudo/Estruturas de dados/arvore_binaria.c
@@ -18,13 +18,24 @@ NoArv *insert_1(NoArv *raiz, int num){
   }
   else{
     if (raiz->num < num) {
-      raiz->esquerda = insert_1(raiz->esquerda, num);
-    }
-    else{
       raiz->direita = insert_1(raiz->direita, num);
     }
+    else{
+      raiz->esquerda = insert_1(raiz->esquerda, num);
+    }
     return raiz;
-  } 
+  }
+}
+
+
+void adicionar_num_vetor (int num, int vetor_arvore[]){
+  if (vetor_arvore[0] == 0){
+    vetor_arvore[1] = num;
+  }
+  else{
+    vetor_arvore[vetor_arvore[0] + 1] = num;
+  }
+  vetor_arvore[0] += 1; 
 }
 
 void imprimir_erd(NoArv *raiz){
@@ -35,18 +46,62 @@ void imprimir_erd(NoArv *raiz){
   }
 }
 
+void alocar_erd(NoArv *raiz, int vetor_arvore[]){
+  if (raiz){
+    alocar_erd(raiz->esquerda, vetor_arvore);
+    adicionar_num_vetor(raiz->num, vetor_arvore);
+    alocar_erd(raiz->direita, vetor_arvore);
+  }
+}
+
+int altura(NoArv *raiz){
+  int esq, dir;
+    if(raiz == NULL){
+        return -1;
+    }
+    else{
+        esq = altura(raiz->esquerda);
+        dir = altura(raiz->direita);
+        if(dir < esq){
+            return ++esq;
+        }
+        else{
+            return ++dir;
+        }
+    }
+}
+
+int qtd_elementos(NoArv *raiz){
+  if (raiz){
+    return 1 + qtd_elementos(raiz->esquerda) + qtd_elementos(raiz->direita);
+  }
+  else{
+    return 0;
+  }
+}
+
 int main(){
   NoArv *raiz = NULL;
-  int num = -2;
+  int* vetor_arvore = NULL;
+  int num = 1;
   
-  while (num != 0) {
+  while (num > 0) {
     printf("Digite um número para adicionar a árvore: ");
     scanf("%d", &num);
     if (num != -1) {
       raiz = insert_1(raiz, num); 
     }
-    else{
-      imprimir_erd(raiz);
+
+    //imprimir a árvore
+    else if (num == -1){
+      vetor_arvore = (int*)malloc((qtd_elementos(raiz)+1) * sizeof(int));
+      alocar_erd(raiz, vetor_arvore);
+      for (int i = 1; i <= qtd_elementos(raiz); i++){
+        printf("%d ", vetor_arvore[i]);
+      }
+    }
+    else if (num == 0){
+      printf("Altura da árvore: %d\n", altura(raiz));
     }
   }
 
